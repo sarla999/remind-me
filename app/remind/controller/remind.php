e4b1bbf2d583e785778bcf538f227215944fa567
sarla999 <sarla_5233@hotmail.com>
2015-07-21 16:05:19 +0800

    test update

---
+++ app/remind/controller/remind.php
@@ -0,0 +1,34 @@
+<?php
+
+	class remind_controller_remind
+	{
+	
+		public function __construct()
+		{
+			//core_templates::view('test.html');
+			//echo 'this is a test index';
+
+		}
+		
+		public function index()
+		{
+
+			echo 'This is index';
+
+		}
+
+		public function lists()
+		{
+
+			echo 'This is lists';
+
+		}
+		
+
+
+
+	}
+
+
+
+
