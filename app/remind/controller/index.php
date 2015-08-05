a9648fdcccc8281f5d0ba3992d12dbfe0a2555de
sarla999 <sarla_5233@hotmail.com>
2015-08-05 15:38:27 +0800

    add new func

--- app/remind/controller/index.php
+++ app/remind/controller/index.php
@@ -5,11 +5,19 @@
 	
 		public function __construct()
 		{
-			core_view::views('test.html');
+			//core_templates::view('test.html');
+
+			$str = libs_tools::encrypt('shiyili@eyou.com');
+
+			echo $str;
+			echo '<br>';
+			$str1 = libs_tools::decrypt($str);
+			echo $str1;
+		
 
 		}
 
-		
+		public function index(){}	
 
 
 
