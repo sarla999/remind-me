d47ae1ff23ef0fc2cbd30f651daa88ce78923ca9
sarla999 <sarla_5233@hotmail.com>
2015-08-10 11:33:17 +0800

    add feedback

---
+++ app/remind/controller/feedback.php
@@ -0,0 +1,48 @@
+<?php
+/*
+*用户反馈 controller
+*/
+class remind_controller_feedback
+{
+
+
+
+	public function index()
+	{
+
+		echo 'this is a new func';
+
+	}
+
+
+	public function addFeedback()
+	{
+		 if(isset($_GET['username']) && !empty($_GET['username']) && isset($_GET['comment']) && !empty($_GET['comment'])){
+
+			echo '提交参数错误';
+		}
+
+		$feedback = array();
+		if($_GET['uid']){
+			$feedback['uid'] = $_GET['uid'];
+		}else{
+			$_GET['uid'] = 0;
+		}
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
